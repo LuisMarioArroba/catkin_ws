9e366441e74f75f7aef4537451eca1b28b3698cf
LuisMarioArroba <lmarroba@espol.edu.ec>
2023-01-18 13:36:42 -0500

    Agregando codigo

---
+++ build/conveyor_demo/src/gazebo-conveyor/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/catkin_ws/src/conveyor_demo/src/gazebo-conveyor
+# Build directory: /home/user/catkin_ws/build/conveyor_demo/src/gazebo-conveyor
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ devel/share/gazebo_conveyor/cmake/gazebo_conveyor-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(gazebo_conveyor_MESSAGE_FILES "/home/user/catkin_ws/src/conveyor_demo/src/gazebo-conveyor/msg/ConveyorBeltState.msg")
+set(gazebo_conveyor_SERVICE_FILES "/home/user/catkin_ws/src/conveyor_demo/src/gazebo-conveyor/srv/ConveyorBeltControl.srv")
